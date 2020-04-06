ec81b5adbdf28a661f0f2f9a56da53a5ce3b9759
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-06 00:08:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+ end
+def batch_badges_creator
+  batch_badges_creator.each do |individual_part_of_array|
+individual_part_of_array
+end
+  
